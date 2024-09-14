cfb65e8abf287879af17db96918a196375673561
VITOR VINICIUS LIMA SILVA <vitor14_df@hotmail.com>
2024-09-14 13:00:00 -0300

    14/09

---
+++ scr.js
@@ -0,0 +1,23 @@
+var botao = document.getElementById('men')
+var itens = document.getElementById('itens')
+
+
+function menuExpandir(){
+    if(itens.style.display == 'block'){
+        itens.style.display = 'none'
+    }else{
+        itens.style.display = 'block'
+    }
+}
+
+function mudouTamanho(){
+    if(window.innerWidth >= 768){
+        itens.style.display = 'block'
+    }else{
+        itens.style.display = 'none'
+    }
+}
+
+
+
+botao.addEventListener('click', menuExpandir)
